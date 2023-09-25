ce52c7630f53a16accd1124abbc653f3c2e05875
Denis <deniskuz@pm.me>
2023-09-25 23:20:01 +0300

    test rsa

--- src/test.py
+++ src/test.py
@@ -1,5 +1,33 @@
-from des import DesKey
-import donna25519
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization, hashes
 
 if __name__ == "__main__":
-    print('kek')
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+    )
+    public_key = private_key.public_key()
+
+    test_string = "бебра"
+    data = test_string.encode('utf-8')
+    # print(uni_string.decode('utf-8'))
+
+    encrypted_data = public_key.encrypt(
+        data,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    print(encrypted_data)
+
+    decrypted_data: str = private_key.decrypt(
+        encrypted_data,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    print(decrypted_data.decode('utf-8'))
